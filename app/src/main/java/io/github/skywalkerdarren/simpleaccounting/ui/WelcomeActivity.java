70d02b06d99fda63e6ce49962ba671aa49f71a26
Darren <yh322yh@163.com>
2018-03-31 14:35:52 +0800

    全屏

--- app/src/main/java/io/github/skywalkerdarren/simpleaccounting/ui/WelcomeActivity.java
+++ app/src/main/java/io/github/skywalkerdarren/simpleaccounting/ui/WelcomeActivity.java
@@ -1,13 +1,13 @@
 package io.github.skywalkerdarren.simpleaccounting.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 
 import io.github.skywalkerdarren.simpleaccounting.R;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
