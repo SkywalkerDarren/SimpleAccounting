e7be09782dfb9ae57cdf44bdc572bdc672401a3f
Darren <yh322yh@163.com>
2018-04-08 23:17:08 +0800

    修复命名

--- app/src/main/java/io/github/skywalkerdarren/simpleaccounting/adapter/StatsAdapter.java
+++ app/src/main/java/io/github/skywalkerdarren/simpleaccounting/adapter/StatsAdapter.java
@@ -17,19 +17,19 @@ import io.github.skywalkerdarren.simpleaccounting.view_model.StatsItemViewModel;
  * @date 2018/3/14
  */
 
-public class StatsAdapter extends BaseDataBindingAdapter<StatsLab.Stats, ItemStatsBinding> {
+public class StatsAdapter extends BaseDataBindingAdapter<StatsLab.BillStats, ItemStatsBinding> {
 
     /**
      * 统计适配器
      *
      * @param data 统计类列表
      */
-    public StatsAdapter(@NonNull List<StatsLab.Stats> data) {
+    public StatsAdapter(@NonNull List<StatsLab.BillStats> data) {
         super(R.layout.item_stats, data);
     }
 
     @Override
-    protected void convert(ItemStatsBinding binding, StatsLab.Stats item) {
+    protected void convert(ItemStatsBinding binding, StatsLab.BillStats item) {
         binding.setStats(new StatsItemViewModel(item, getParentPosition(item) + 1));
     }
 }
