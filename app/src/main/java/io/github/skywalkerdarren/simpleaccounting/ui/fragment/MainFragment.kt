eef358c268bfd9bc1a4249cfbe07a47744cced33
SkywalkerDarren <yh322yh@163.com>
2020-01-06 17:01:28 +0800

    refactor main activity

--- app/src/main/java/io/github/skywalkerdarren/simpleaccounting/ui/fragment/MainFragment.kt
+++ app/src/main/java/io/github/skywalkerdarren/simpleaccounting/ui/fragment/MainFragment.kt
@@ -30,6 +30,7 @@ class MainFragment : Fragment() {
     private val mDiscoveryFragment = DiscoveryFragment.newInstance()
     private lateinit var mViewPager: ViewPager
     private lateinit var mAddBillButton: FloatingActionButton
+    private lateinit var navigation: BottomNavigationView
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -51,6 +52,30 @@ class MainFragment : Fragment() {
         false
     }
 
+    private val listener = object : OnPageChangeListener {
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+        override fun onPageSelected(position: Int) {
+            when (position) {
+                0 -> {
+                    navigation.selectedItemId = R.id.navigation_stats
+                    buttonAnimator(mAddBillButton, true).start()
+                }
+                1 -> {
+                    navigation.selectedItemId = R.id.navigation_bill
+                    buttonAnimator(mAddBillButton, false).start()
+                }
+                2 -> {
+                    navigation.selectedItemId = R.id.navigation_discovery
+                    buttonAnimator(mAddBillButton, true).start()
+                }
+                else -> {
+                }
+            }
+        }
+
+        override fun onPageScrollStateChanged(state: Int) {}
+    }
+
     fun newIntent(context: Context?): Intent? {
         return Intent(context, MainActivity::class.java)
     }
@@ -59,7 +84,7 @@ class MainFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_bill, container, false)
 
         val binding = FragmentBillBinding.bind(root)
-        val navigation = binding.navigation
+        navigation = binding.navigation
         mViewPager = binding.contentViewPager
         mAddBillButton = binding.addBillFab
         // 点击增加按钮事件
@@ -89,44 +114,31 @@ class MainFragment : Fragment() {
                 }
             }
         }
-        mViewPager.addOnPageChangeListener(object : OnPageChangeListener {
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
-            override fun onPageSelected(position: Int) {
-                when (position) {
-                    0 -> {
-                        navigation.selectedItemId = R.id.navigation_stats
-                        buttonAnimator(mAddBillButton, true).start()
-                    }
-                    1 -> {
-                        navigation.selectedItemId = R.id.navigation_bill
-                        buttonAnimator(mAddBillButton, false).start()
-                    }
-                    2 -> {
-                        navigation.selectedItemId = R.id.navigation_discovery
-                        buttonAnimator(mAddBillButton, true).start()
-                    }
-                    else -> {
-                    }
-                }
-            }
-
-            override fun onPageScrollStateChanged(state: Int) {}
-        })
         mViewPager.currentItem = 1
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mViewPager.addOnPageChangeListener(listener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mViewPager.removeOnPageChangeListener(listener)
     }
 
     /**
      * 按钮动画
      *
      * @param view     视图
-     * @param disapper 是否消失
+     * @param disappear 是否消失
      * @return 动画组
      */
-    private fun buttonAnimator(view: View?, disapper: Boolean): AnimatorSet {
-        val start: Float = if (disapper) 1f else 0f
-        val end: Float = if (disapper) 0f else 1f
+    private fun buttonAnimator(view: View?, disappear: Boolean): AnimatorSet {
+        val start: Float = if (disappear) 1f else 0f
+        val end: Float = if (disappear) 0f else 1f
         val alpha = ObjectAnimator.ofFloat(view, "alpha", start, end)
         val rotation = ObjectAnimator.ofFloat(view, "rotation", 0f, 360f)
         val scaleX = ObjectAnimator.ofFloat(view, "scaleX", start, end)
@@ -134,7 +146,7 @@ class MainFragment : Fragment() {
         val set = AnimatorSet()
         set.playTogether(alpha, rotation, scaleX, scaleY)
         set.duration = 250
-        if (disapper) {
+        if (disappear) {
             set.interpolator = AccelerateInterpolator()
         } else {
             set.interpolator = DecelerateInterpolator()
@@ -142,14 +154,14 @@ class MainFragment : Fragment() {
         set.addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: Animator) {
                 super.onAnimationEnd(animation)
-                if (disapper) {
+                if (disappear) {
                     view?.visibility = View.INVISIBLE
                 }
             }
 
             override fun onAnimationStart(animation: Animator) {
                 super.onAnimationStart(animation)
-                if (!disapper) {
+                if (!disappear) {
                     view?.visibility = View.VISIBLE
                 }
             }
